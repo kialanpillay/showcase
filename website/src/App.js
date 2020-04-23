930cf6f3d18c3c6cf175b8a2bd9d3c462baa4093
Kialan Pillay <kialanpillay@gmail.com>
2020-04-23 03:13:10 +0200

    Initial Home Page

--- website/src/App.js
+++ website/src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+import Image from "./Image.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar id="nav" variant="dark" bg="dark">
+          <Navbar.Brand href="/">
+            <p id="logo"> &#60;KIALAN/&#62;</p>
+          </Navbar.Brand>
+        </Navbar>
+        <div className="body">
+          <Container style={{ marginTop: "4rem" }}>
+            <Row
+              style={{
+                width: "100%",
+                color: "white",
+                alignItems: "left",
+                backgroundColor: "transparent",
+              }}
+            >
+              <Col md="auto">
+                <h1 class="title">Hi, I'm Kialan</h1>
+                <h1 class="subtitle">Welcome to my showcase.</h1>
+              </Col>
+              <Col md="auto">
+                <div className="vl"></div>
+              </Col>
+              <Col md="auto">
+                <Image />
+              </Col>
+            </Row>
+            <Row
+              style={{
+                width: "100%",
+                marginTop: "10vh",
+              }}
+            >
+              <Col md="auto">
+                <Button style={{ width: "13vw"}} variant="primary" size="lg">
+                  <h3 className="buttonText">Intro</h3>
+                </Button>
+              </Col>
+              <Col md="auto">
+                <h1 className="arrow">&#8594;</h1>
+              </Col>
+              <Col md="auto">
+                <Button style={{ width: "13vw" }} variant="warning" size="lg">
+                <h3 className="buttonText">XP</h3>
+                </Button>
+              </Col>
+              <Col md="auto">
+                <h1 className="arrow">&#8594;</h1>
+              </Col>
+              <Col md="auto">
+                <Button style={{ width: "13vw"}} variant="danger" size="lg">
+                <h3 className="buttonText">Projects</h3>
+                </Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ website/src/Image.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from "react";
+import img from "./avatar.png";
+import Tooltip from "react-bootstrap/Tooltip";
+import Overlay from "react-bootstrap/Overlay";
+
+function Image() {
+  const [show, setShow] = useState(false);
+  const target = useRef(null);
+
+  return (
+    <>
+      <a
+        href="https://kialan.co.za"
+        target="_blank"
+        ref={target}
+        onMouseEnter={() => setShow(!show)}
+      >
+        <img
+          width="250vw"
+          src={img}
+          alt="Profile Picture"
+          style={{
+            marginLeft: "2vw",
+            borderRadius: "50%",
+            filter: "grayscale(100%)",
+          }}
+        ></img>
+      </a>
+      <Overlay target={target.current} show={show} placement="right">
+        {(props) => (
+          <Tooltip id="overlay-example" {...props}>
+            Bonus Content! Click to visit my digital resume; a static site built with Gatsby.
+          </Tooltip>
+        )}
+      </Overlay>
+    </>
+  );
+}
+
+export default Image;
