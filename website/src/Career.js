52825569c5d64ee0ea2070a538636e9ea76c41a0
Kialan Pillay <kialanpillay@gmail.com>
2020-07-09 18:23:35 +0200

    Career Content Update/Refactor

--- website/src/Career.js
+++ website/src/Career.js
@@ -11,6 +11,7 @@ import js from "./assets/javascript.svg";
 import python from "./assets/python.png";
 import golang from "./assets/golang.png";
 import dynamodb from "./assets/dynamodb.png";
+import sql from "./assets/mysql.svg";
 
 function Career() {
   return (
@@ -28,107 +29,24 @@ function Career() {
           </h1>
         </Col>
       </Row>
-      {/*
-      <Row>
-        <Col
-          md={3}
-          xs={12}
-          style={{
-            marginRight: "4vw",
-          }}
-        >
-          <Card
-            id="careerWall"
-            style={{
-              width: "100%",
-              height: "22rem",
-              textAlign: "left",
-              backgroundColor: "transparent",
-            }}
-            border="light"
-          >
-            <Card.Body>
-              <Card.Title>
-                <h5>Wall of Fame</h5>
-              </Card.Title>
-              <div></div>
-              <div>
-                <Row className="justify-content-md-center">
-                  <img
-                    id="aws"
-                    style={{
-                      marginTop: "2vw",
-                    }}
-                    width={160}
-                    src={aws}
-                    alt="AWS Logo"
-                  />
-                </Row>
-              </div>
-              <div>
-                <Row className="justify-content-md-center">
-                  <img
-                    id="electrum"
-                    style={{
-                      marginTop: "2vw",
-                    }}
-                    width={180}
-                    src={electrum}
-                    alt="Electrum Logo"
-                  />
-                </Row>
-              </div>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col
-          md={4}
-          xs={12}
-          style={{
-            marginRight: "2vw",
-            marginBottom: "2vh",
-          }}
-        >
-          <Row>
-            <h1 className="careerNumeric">5</h1>
-            <h1 className="careerSubtitle">Internships</h1>
-          </Row>
-          <Row>
-            <h1 className="careerNumeric">3</h1>
-            <h1 className="careerSubtitle">Companies</h1>
-          </Row>
-        </Col>
-        <Col
-          md={3}
-          xs={12}
-          style={{ 
-            marginBottom: "2vh",
-          }}
-        >
-          <Row>
-            <h1 className="careerNumeric">5</h1>
-            <h1 className="careerSubtitle">Projects</h1>
-          </Row>
-          <Row>
-            <h1 className="careerNumeric">7</h1>
-            <h1 className="careerSubtitle">Months</h1>
-          </Row>
-        </Col>
-        </Row>*/}
       <Row
         style={{
           marginBottom: "6vh",
         }}
       >
-        <Col md={12} sm={12} style={{
-          marginLeft: "1vh",
-          marginRight: "1vh",
-        }}>
+        <Col
+          md={12}
+          sm={12}
+          style={{
+            marginLeft: "1vh",
+            marginRight: "1vh",
+          }}
+        >
           <Carousel
             style={{
               marginTop: "2vw",
               width: "100%",
-              height: "30rem"
+              height: "30rem",
             }}
           >
             <Carousel.Item>
@@ -146,7 +64,7 @@ function Career() {
                   <Card.Body>
                     <Card.Title>I N F O</Card.Title>
                     <Row>
-                      <Col md="auto">
+                      <Col md={8}>
                         <div>
                           <h1>Amazon Web Services</h1>
                           <h5>Software Development Engineer Intern</h5>
@@ -161,29 +79,41 @@ function Career() {
                         <Row
                           className="justify-content-md-center"
                           style={{
-                            marginTop: "2vw",
+                            marginTop: "1vw",
                           }}
                         >
-                          <h5 className="stackSubtitle">TECHNOLOGY STACK</h5>
-                        </Row>
-                        <Row
-                          className="justify-content-md-center"
-                          style={{
-                            marginBottom: "3vw",
-                          }}
-                        >
-                          <img className="stackImage" id="stackReact" width="60" src={react} alt="react"></img>
-                          <img className="stackImage" width="40" src={ts} alt="typescript"></img>
-                          <img className="stackImage" width="60" src={dynamodb} alt="dynamodb"></img>
-                          <img className="stackImage" width="40" src={golang} alt="golang"></img>
-                          <img className="stackImage" width="42" src={python} alt="python"></img>
+                          <img
+                            className="stackImage"
+                            id="stackReact"
+                            width="60"
+                            src={react}
+                            alt="react"
+                          ></img>
+                          <img
+                            className="stackImage"
+                            width="40"
+                            src={ts}
+                            alt="typescript"
+                          ></img>
+                          <img
+                            className="stackImage"
+                            width="60"
+                            src={dynamodb}
+                            alt="dynamodb"
+                          ></img>
+                          <img
+                            className="stackImage"
+                            width="40"
+                            src={golang}
+                            alt="golang"
+                          ></img>
                         </Row>
                       </Col>
                     </Row>
                     <div>
                       <h5 className="careerText">
                         Reduced operational load for the Managed Deployments
-                        team by developing and implemented an information
+                        team by developing and implementing an information
                         linking and tracking system. Typescript and React was
                         used for the solution front-end, a Golang RESTful API
                         server backend, and a DynamoDB repository for
@@ -218,7 +148,7 @@ function Career() {
                   <Card.Body>
                     <Card.Title>I N F O</Card.Title>
                     <Row>
-                      <Col md="auto">
+                      <Col md={8}>
                         <div>
                           <h1>Electrum Payments</h1>
                           <h5>Software Developer Intern</h5>
@@ -233,19 +163,22 @@ function Career() {
                         <Row
                           className="justify-content-md-center"
                           style={{
-                            marginTop: "2vw",
+                            marginTop: "1vw",
                           }}
                         >
-                          <h5 className="stackSubtitle">TECHNOLOGY STACK</h5>
-                        </Row>
-                        <Row
-                          className="justify-content-md-center"
-                          style={{
-                            marginBottom: "3vw",
-                          }}
-                        >
-                          <img className="stackImage" id="stackReact" width="60" src={react} alt="react"></img>
-                          <img className="stackImage" width="40" src={js} alt="javascript"></img>
+                          <img
+                            className="stackImage"
+                            id="stackReact"
+                            width="60"
+                            src={react}
+                            alt="react"
+                          ></img>
+                          <img
+                            className="stackImage"
+                            width="40"
+                            src={js}
+                            alt="javascript"
+                          ></img>
                         </Row>
                       </Col>
                     </Row>
@@ -281,7 +214,7 @@ function Career() {
                   <Card.Body>
                     <Card.Title>I N F O</Card.Title>
                     <Row>
-                      <Col md="auto">
+                      <Col md={8}>
                         <div>
                           <h1>Nomanini</h1>
                           <h5>Software Developer Intern</h5>
@@ -292,10 +225,33 @@ function Career() {
                           </h5>
                         </div>
                       </Col>
+                      <Col>
+                        <Row
+                          className="justify-content-md-center"
+                          style={{
+                            marginTop: "1vw",
+                          }}
+                        >
+                          <img
+                            className="stackImage"
+                            width="40"
+                            src={python}
+                            alt="python"
+                          ></img>
+                          <img
+                            className="stackImage"
+                            width="60"
+                            src={sql}
+                            alt="mysql"
+                          ></img>
+                        </Row>
+                      </Col>
                     </Row>
                     <div>
                       <h5 className="careerText">
-                        Content Loading...
+                        Developed a Python package to extract and process data relevant
+                        to merchant health, to generate a set of credit scores
+                        using a heuristic algorithm. Conducted data analysis on the extracted information using several machine learning techniques, including clustering and neural networks, with Keras.
                       </h5>
                     </div>
                   </Card.Body>
